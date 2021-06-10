5d030ec84a99314ab59442a8216b3df42b414522
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-06-10 11:33:26 -0300

    Lendo uma cadeia de caracteres e transaformando em um array de bytes e decodificando usando o Encoding.UTFO

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,18 +1,41 @@
-﻿using ByteBankImportacaoExportacao.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
             Console.ReadLine();
         }
+
+        private static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = Encoding.UTF8;
+
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
     }
-} 
- 
+}
