f96156123570b0ac1874f7219ff8ac10655a9cee
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-06-10 11:47:00 -0300

    Usando o bloco using para liberar o uso do arquivo

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -10,16 +10,17 @@ namespace ByteBankImportacaoExportacao
         {
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            var buffer = new byte[1024];
-            var numeroDeBytesLidos = -1;
-
-            while(numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
-            }
+                var buffer = new byte[1024];
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer);
+                }
+            }            
 
             Console.ReadLine();
         }
@@ -30,12 +31,6 @@ namespace ByteBankImportacaoExportacao
 
             var texto = utf8.GetString(buffer);
             Console.Write(texto);
-
-            //foreach (var meuByte in buffer)
-            //{
-            //    Console.Write(meuByte);
-            //    Console.Write(" ");
-            //}
         }
     }
 }
